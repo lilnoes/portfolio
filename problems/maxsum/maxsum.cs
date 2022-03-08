530cbd8ff1e86c9539c858f66c8bfca78ec4a218
leon <ishimweleon@gmail.com>
2022-03-08 11:19:14 +0300

    infile

--- problems/maxsum/maxsum.cs
+++ problems/maxsum/maxsum.cs
@@ -1,14 +1,31 @@
+/*
+Author: Leon Emmanuel ISHIMWE
+*/
 using System;
-using System.IO;
 using System.Collections.Generic;
 
-class MaxSum
+public class MaxSum
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        string lines = @"215
+193 124
+117 237 442
+218 935 347 235
+320 804 522 417 345
+229 601 723 835 133 124
+248 202 277 433 207 263 257
+359 464 504 528 516 716 871 182
+461 441 426 656 863 560 380 171 923
+381 348 573 533 447 632 387 176 975 449
+223 711 445 645 245 543 931 532 937 541 444
+330 131 333 928 377 733 017 778 839 168 197 197
+131 171 522 137 217 224 291 413 528 520 227 229 928
+223 626 034 683 839 053 627 310 713 999 629 817 410 121
+924 622 911 233 325 139 721 218 253 223 107 233 230 124 233";
         long ans = 0;
         List<List<int>> numbers = new List<List<int>>();
-        foreach (string line in File.ReadLines("input.txt")) numbers.Add(getNumbers(line));
+        foreach (string line in lines.Split('\n')) numbers.Add(getNumbers(line));
 
         //initialise a lookup table filled with 0
         long[][] table = new long[numbers.Count][];
